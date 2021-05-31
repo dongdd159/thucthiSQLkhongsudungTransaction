6a2081a9dc752b39c671039f4d60d64e71cb12d2
dongdd159 <79952800+dongdd159@users.noreply.github.com>
2021-05-31 22:57:18 +0700

    first commit+

---
+++ src/main/java/dao/IUserDAO.java
@@ -0,0 +1,34 @@
+package dao;
+
+import model.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDAO {
+    public void insertUser(User user) throws SQLException;
+
+    public User selectUser(int id);
+
+    public List<User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(User user) throws SQLException;
+
+    public List<User> selectbyCountry(String search);
+
+    public List<User> orderList();
+
+    public List<User> orderSelectedList(String search);
+
+    User getUserById(int id);
+
+    void insertUserStore(User user) throws SQLException;
+
+    void addUserTransaction(User user, int[] permision);
+
+    public void insertUpdateWithoutTransaction();
+
+
+}
